c26e405113fd2e959a69876fe3c9a5b26b43f01d
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-05-24 13:13:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = "ORLEN1024";
-const CACHE_NAME = "orlen-gaz-cache-ORLEN1024";
+const SW_VERSION = "ORLEN1025";
+const CACHE_NAME = "orlen-gaz-cache-ORLEN1025";
 const APP_SHELL = [
   "./",
   "./index.html",
